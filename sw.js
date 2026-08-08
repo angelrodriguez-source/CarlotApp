b53d6fd5b7af6dda4fca1ec86d733acfcba73d5f
angelrodriguez-source <angelrodriguez-source@users.noreply.github.com>
2026-08-08 12:52:03 +0000

    deploy: a6130d657dec057145ed6e25faf097aef8458163

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *  - Assets con hash de Vite (/assets/): cache primero (son inmutables)
  *  - Resto: red con fallback a cache
  */
-const CACHE = 'carlotapp-v3'
+const CACHE = 'carlotapp-v4'
 
 // Estaticos de public/ (sin hash): precacheados para que el avatar y los
 // iconos de la navegacion funcionen offline desde el primer arranque
@@ -23,6 +23,7 @@ const PRECACHE = [
   './icono-toma.png',
   './icono-sueno.png',
   './icono-sueno-post.png',
+  './icono-objetivo-sueno.png',
   './icono-bano.png',
   './icono-vitamina.png',
   './icono-medicacion.png',
